7c3d436ba4d040cfbec42917b2a8dd58e06cbedb
NeiJunio <neijunio50@gmail.com>
2026-05-16 13:17:06 -0300

    Adequeção da UX da tela de login para o usuário conseguir visualizar a senha que está digitando

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -4,13 +4,14 @@ import { useState } from "react"
 
 import { useLogin } from "@/hooks/useLogin.js"
 
-import { ChefHat, Lock, Mail } from "lucide-react"
+import { ChefHat, Lock, Mail, Eye, EyeOff } from "lucide-react"
 
 import styles from './page.module.css'
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const { handleLogin, loading } = useLogin();
 
@@ -53,13 +54,21 @@ export default function Login() {
                             <Lock size={20} className={styles.icon} />
                             <input
                                 id="senha"
-                                type="password"
+                                type={showPassword ? 'text' : 'password'}
                                 placeholder="••••••••"
                                 value={senha}
                                 onChange={(e) => setSenha(e.target.value)}
                                 required
                                 disabled={loading}
                             />
+                            <button
+                                type="button"
+                                className={styles.eyeButton}
+                                onClick={() => setShowPassword(!showPassword)}
+                                tabIndex="-1" // Evita que o tab pare no ícone antes do botão de login
+                            >
+                                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </button>
                         </div>
                     </div>
 
